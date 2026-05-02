fc337c8db0ef219c524a0af3a9c65736d6bb86a0
rajsekharde <rajsekharde612@gmail.com>
2026-05-02 08:19:03 +0530

    Add basic HTTP server

--- api/main.go
+++ api/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
+var PORT int = 8000
+var PORT_STR = ":" + strconv.Itoa(PORT)
+
+func HandleRoot(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello")
+	log.Printf("%s %s %d\n", r.Method, r.URL.Path, http.StatusOK)
+}
+
 func main() {
-	fmt.Println("Hello")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", HandleRoot)
+
+	log.Printf("Server running on port %d\n", PORT)
+	log.Fatal(http.ListenAndServe(PORT_STR, mux))
 }
