11a75ede43df45eaec18954d1a299ac8f2320e36
rajsekharde <rajsekharde612@gmail.com>
2026-05-16 22:09:55 +0530

    Add CD pipeline

--- api/api_test.go
+++ api/api_test.go
@@ -8,7 +8,7 @@ func TestLargest(t *testing.T) {
 	result := ReturnLargest(10, 15)
 	expected := 15
 
-	if result == expected {
+	if result != expected {
 		t.Errorf("Largest(10, 15) = %d; Expected = %d", result, expected)
 	}
 }
